b7eb4d3ac7477558fcd5bb6397fba849384e0e0a
ktochow1 <ktochow1@binghamton.edu>
2019-01-31 18:37:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,8 @@ require 'pry'
   end
 
   def self.create_from_collection(students_array)
+    #students_array is an array of hashes with name, location of each student 
+    #look at students_array and pic each student hash and create a new student from that student_hash
     students_array.each do |student_hash|
      Student.new(student_hash) 
     end
