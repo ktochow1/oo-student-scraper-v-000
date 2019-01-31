67c6935da41859b54cfd5184de9dd52ba39ff313
ktochow1 <ktochow1@binghamton.edu>
2019-01-31 18:48:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ require 'pry'
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |k,v| 
-      self.send("#{k}=", "#{v)}" }
+      self.send("#{k}=", v)
       @@all << self
     end
   end
