20f83fc5418e52c2cf172ad8aaab2fd221040a5d
ktochow1 <ktochow1@binghamton.edu>
2019-01-31 18:28:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ require 'pry'
     # binding.pry
     self.send("name=", student_hash[:name])
     self.send("location=", student_hash[:location])
-    self.send("profile_url=", student_hash[:profile_url])
+    # self.send("profile_url=", student_hash[:profile_url])
     @@all << self
   end
   
