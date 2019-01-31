d6d9430f94b82c4b07cd568325f54f41e5d93f99
ktochow1 <ktochow1@binghamton.edu>
2019-01-31 18:38:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -55,39 +55,5 @@ class Scraper
   profile_hash[:bio] = scrape_page.css("div.description-holder p").text
   profile_hash
   end
-
 end
-
-
-# def self.scrape_profile_page(profile_url)
-#     page = Nokogiri::HTML(open(profile_url))
-#     student = {}
-#     container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
-#     container.each do |link|
-#       if link.include?("twitter")
-#         student[:twitter] = link
-#       elsif link.include?("linkedin")
-#         student[:linkedin] = link
-#       elsif link.include?("github")
-#         student[:github] = link
-#       elsif link.include?(".com")
-#         student[:blog] = link
-#       end
-#     end
-#     student[:profile_quote] = page.css(".profile-quote").text
-#     student[:bio] = page.css("div.description-holder p").text
-#     student
-#   end
-# end
-
-# iterate through the container, collecting the twitter/ blog/ linkedin info
-# ... 
-
-# if link.include?("twitter")
-#           student[:twitter] = link
-#         elsif link.include?("linkedin")
-#           student[:linkedin] = link
-
-# 1. Scrapped page with nokogiri. 2. Made a new, empty hash. 3. To add keys/ values to the hash, I iterated through the container if the value contains mention of linkedin/ github/ etc.
-# end
 
